ccae914fe0453fa70cee7b1410cf6391323c7cc8
000Andre000 <a.fernandes1808@gmail.com>
2025-09-20 22:54:25 +0530

    update url 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://stupendous-rolypoly-ffe40d.netlify.app/login',
+      'https://stupendous-rolypoly-ffe40d.netlify.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
